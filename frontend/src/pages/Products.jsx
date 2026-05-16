2274e284ac273d248cd470ed6b770cfda78c07a0
Githu-bamitcode <mail.amit.banerjee@gmail.com>
2026-05-16 16:44:30 +0530

    Updated version as on 16052026
    Updated version as on 16052026

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -30,7 +30,6 @@ const Products = () => {
       setLoading(true);
       const res = await axios.get(
         `${import.meta.env.VITE_API_URL}/api/v1/product/getallproducts`,
-
         //        `http://localhost:8000/api/v1/product/getallproducts`,
       );
       console.log(import.meta.env.VITE_API_URL);
